53f1a8b3957f5cd64c04fec534d1a34035ff83ef
Vitor Cardoso <vitorcardoso.desenvolvedor@gmail.com>
2017-09-05 22:56:57 -0300

    Update Script DB
    Atualização Create Tabelas

--- src/main/java/conexaoBanco/ScriptBanco.sql
+++ src/main/java/conexaoBanco/ScriptBanco.sql
@@ -8,48 +8,56 @@
  * Created: Mar 24, 2017
  */
 
-CREATE TABLE paciente
-(
-  id int auto_increment,
-  nome character varying (20) NOT NULL,
-  sobrenome character varying (20) NOT NULL,
-  email character varying (30) NOT NULL,
-  senha character varying (20) NOT NULL,
-  rg character varying (15) NOT NULL,
-  cpf character varying (15) NOT NULL,
-  endereco character varying (30) NOT NULL,
-  ativo character (2) NOT NULL,
-  CONSTRAINT pk_paciente_id PRIMARY KEY (id)
-);
+CREATE TABLE `paciente` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(20) NOT NULL,
+  `sobrenome` varchar(20) NOT NULL,
+  `login` varchar(20) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `senha` varchar(20) NOT NULL,
+  `rg` varchar(15) NOT NULL,
+  `cpf` varchar(15) NOT NULL,
+  `endereco` varchar(30) NOT NULL,
+  `bairro` varchar(50) NOT NULL,
+  `cidade` varchar(50) NOT NULL,
+  `estado` varchar(50) NOT NULL,
+  `ativo` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
-CREATE TABLE agendamento
-(
-  id int auto_increment,
-  datadose date NOT NULL,
-  idpaciente int NOT NULL,
-  idvacinas int NOT NULL,
-  ativo boolean not null,
-  CONSTRAINT pk_agendamento_id PRIMARY KEY (id),
-  CONSTRAINT fk_paciente_id FOREIGN KEY (idpaciente) REFERENCES paciente (id) on delete cascade on update cascade,
-  CONSTRAINT fk_vacinas_id FOREIGN KEY (idvacinas) REFERENCES vacinas (id) on delete cascade on update cascade
-);
+
+ CREATE TABLE `agendamento` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `dataDose` date NOT NULL,
+  `quantidadeVac` int(11) NOT NULL,
+  `idPaciente` int(11) NOT NULL,
+  `idVacinas` int(11) NOT NULL,
+  `ativo` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_paciente_id` (`idPaciente`),
+  KEY `fk_vacinas_id` (`idVacinas`),
+  CONSTRAINT `fk_paciente_id` FOREIGN KEY (`idPaciente`) REFERENCES `paciente` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_vacinas_id` FOREIGN KEY (`idVacinas`) REFERENCES `vacinas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+SELECT * FROM sgv.usuario;
 
 select * from agendamento;
 
 
-CREATE TABLE vacinas
-(
-  id int auto_increment,
-  datavalidade date NOT NULL,
-  datafabricacao date NOT NULL,
-  nome character varying(100) NOT NULL,
-  tipo character varying(20) NOT NULL,
-  quantidade numeric(5,0) NOT NULL,
-  lote character varying(25) NOT NULL,
-  idlaboratorio numeric(5,0) NOT NULL,
-  ativo character(2) NOT NULL,
-  CONSTRAINT pk_vacinas_id PRIMARY KEY (id)
-);
+CREATE TABLE `vacinas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `dataValidade` date NOT NULL,
+  `dataFabricacao` date NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `tipo` varchar(100) NOT NULL,
+  `quantidade` int(5) NOT NULL,
+  `lote` varchar(25) NOT NULL,
+  `idLaboratorio` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_vacinasLab_id` (`idLaboratorio`),
+  CONSTRAINT `fk_vacinasLab_id` FOREIGN KEY (`idLaboratorio`) REFERENCES `laboratorio` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
 
 CREATE TABLE virtualizacao
 (
@@ -62,39 +70,44 @@ CONSTRAINT fk_pacienteVirt_id FOREIGN KEY (idPaciente) REFERENCES paciente (id) 
 );
 
 
-CREATE TABLE laboratorio
-(
-  id int auto_increment,
-  razaoSocial varchar (100)NOT NULL,
-  cnpj varchar (25)NOT NULL,
-  registroEstadual varchar (25) NOT NULL,
-  nomeFantasia varchar (100) NOT NULL,
-  telefone varchar (25) NOT NULL,
-  site varchar (50),
-  cep varchar (15) NOT NULL,
-  logradouro varchar (50) NOT NULL,
-  numero varchar (6) NOT NULL,
-  bairro varchar (50) NOT NULL,
-  cidade varchar (50) NOT NULL,
-  estado varchar (50) NOT NULL,
-  ativo boolean NOT NULL,
-  CONSTRAINT pk_laboratorio_id PRIMARY KEY (id)
-);
+CREATE TABLE `laboratorio` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `razaoSocial` varchar(100) NOT NULL,
+  `cnpj` varchar(25) NOT NULL,
+  `registroEstadual` varchar(25) NOT NULL,
+  `nomeFantasia` varchar(100) NOT NULL,
+  `telefone` varchar(25) NOT NULL,
+  `site` varchar(50) DEFAULT NULL,
+  `cep` varchar(15) NOT NULL,
+  `logradouro` varchar(50) NOT NULL,
+  `numero` varchar(6) NOT NULL,
+  `bairro` varchar(50) NOT NULL,
+  `cidade` varchar(50) NOT NULL,
+  `estado` varchar(50) NOT NULL,
+  `ativo` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
-CREATE table usuario
-(
-  id int auto_increment,
-  nome varchar (50) NOT NULL,
-  email varchar (50) NOT NULL,
-  login varchar (50) NOT NULL,
-  senha varchar (16) NOT NULL,
-  cargo varchar (50) NOT NULL,
-  rg varchar (25) NOT NULL,
-  cpf varchar (25) NOT NULL,
-  endereco varchar (50) NOT NULL,
-  ativo boolean NOT NULL,
-  CONSTRAINT pk_usuario_id PRIMARY KEY (id)
-);
+
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(20) NOT NULL,
+  `sobrenome` varchar(20) NOT NULL,
+  `login` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `senha` varchar(16) NOT NULL,
+  `cargo` varchar(25) NOT NULL,
+  `rg` varchar(25) NOT NULL,
+  `cpf` varchar(25) NOT NULL,
+  `endereco` varchar(50) NOT NULL,
+  `bairro` varchar(50) NOT NULL,
+  `cidade` varchar(50) NOT NULL,
+  `estado` varchar(50) NOT NULL,
+  `ativo` tinyint(1) NOT NULL,
+  `perfil` varchar(25) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+
 
 CREATE TABLE novosPedidos
 (
